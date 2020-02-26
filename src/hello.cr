707ed38e3801a422e2f524a3ea71d6548afacd3f
Albert Goncalves <goncalves.albert@gmail.com>
2020-02-26 09:46:57 -0500

    getting feet wet

--- src/hello.cr
+++ src/hello.cr
@@ -1,4 +1,13 @@
-"Hello world".each_char do |char|
-  print char
+def f : String
+  "Hello, world!"
 end
-print '\n'
+
+def main
+  x : String = f()
+  x.each_char do |char|
+    print char
+  end
+  print '\n'
+end
+
+main
