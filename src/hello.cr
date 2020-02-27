90678ac5657bdceb37e22789ff238d01043edfaf
Albert Goncalves <goncalves.albert@gmail.com>
2020-02-26 21:04:58 -0500

    far out

--- src/hello.cr
+++ src/hello.cr
@@ -1,9 +1,19 @@
-def f : String
+def f(&block : String -> Nil) : String
+  x = yield "Second?"
+  puts "First"
+  puts x
+  puts yield "Third"
   "Hello, world!"
 end
 
+def g(x : String) : String
+  x
+end
+
 def main
-  x : String = f()
+  x : String = f do |x|
+    g x
+  end
   x.each_char do |char|
     print char
   end
